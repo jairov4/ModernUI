2b0a0b576aa96836f11c703256d0eec58c47750c
jairov <jairo_andres_v@hotmail.com>
2016-10-26 13:02:04 -0500

    Upgraded DMD bin on AppVeyor and commented out the app part to move config to library

--- source/app.d
+++ source/app.d
@@ -3,40 +3,40 @@ module winmain;
 import core.runtime;
 import modernui.core;
 import modernui.ui.control;
-
-version(Windows)
-{
-	import core.sys.windows.windows;
-
-	extern (Windows)
-	int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-	{
-		if(!Runtime.initialize())
-		{
-			return -1;
-		}
-
-		//try
-		//{
-			auto result = myWinMain(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
-			Runtime.terminate();
-			return result;
-		//}
-		//catch (Throwable o) // catch any uncaught exceptions
-		//{
-		//    MessageBoxA(null, cast(char *)o.toString(), "Error", MB_OK | MB_ICONEXCLAMATION);
-		//    return -1;
-		//}
-	}
-
-	int myWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-	{
-		auto wnd = new Window;
-		auto text = new TextElement;
-		wnd.content = text;
-		wnd.show;
-		wnd.messageLoop;
-
-		return 0;
-	}
-}
+//
+//version(Windows)
+//{
+//    import core.sys.windows.windows;
+//
+//    extern (Windows)
+//    int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+//    {
+//        if(!Runtime.initialize())
+//        {
+//            return -1;
+//        }
+//
+//        //try
+//        //{
+//            auto result = myWinMain(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
+//            Runtime.terminate();
+//            return result;
+//        //}
+//        //catch (Throwable o) // catch any uncaught exceptions
+//        //{
+//        //    MessageBoxA(null, cast(char *)o.toString(), "Error", MB_OK | MB_ICONEXCLAMATION);
+//        //    return -1;
+//        //}
+//    }
+//
+//    int myWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+//    {
+//        auto wnd = new Window;
+//        auto text = new TextElement;
+//        wnd.content = text;
+//        wnd.show;
+//        wnd.messageLoop;
+//
+//        return 0;
+//    }
+//}
